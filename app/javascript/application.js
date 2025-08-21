a9c10052d4fb753fd6ec39263c1b97a71704091a
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-08-21 13:24:15 +0200

    commented import bootsrap from app.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,14 @@
 // app/javascript/application.js
 
 // --- Bootstrap ---
-import "bootstrap"
+//import "bootstrap"
 import "@hotwired/turbo-rails"
 
 // --- Stimulus setup ---
 import { Application } from "@hotwired/stimulus"
 //import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-import { application } from "./controllers/index"
+//import { application } from "./controllers/index"
 //import "./controllers" // auto-load all controllers
 
 import "./controllers/index"
