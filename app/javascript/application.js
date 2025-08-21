06e5f333d048e378ee7c45272fcfad381cb86ab9
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-08-21 13:10:05 +0200

    commented stimulus helper from application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ import "@hotwired/turbo-rails"
 
 // --- Stimulus setup ---
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+//import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 import { application } from "./controllers/index"
 //import "./controllers" // auto-load all controllers
