b4945352fc4d231f98b410b3fadc86f98ec2207f
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-08-21 13:31:18 +0200

    new importmap file

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,29 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", preload: true
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js", preload: true
+
+
+
+
+
+
+
+
+
+##THIS WAS THE CURRENT ONE
+
+# Pin npm packages by running ./bin/importmap
+
+#pin "application"
+#pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+#pin "@hotwired/stimulus", to: "stimulus.min.js"
+#pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+#pin_all_from "app/javascript/controllers", under: "controllers"
+
 #pin "bootstrap", to: "bootstrap.bundle.min.js"
 #pin "bootstrap", to: "bootstrap.min.js", preload: true
 #pin "@popperjs/core", to: "popper.js", preload: true
@@ -12,5 +31,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 
 # Pin Bootstrap and Popper via CDN
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", preload: true
-pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js", preload: true
+#pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", preload: true
+#pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js", preload: true
