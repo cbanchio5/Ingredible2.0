4a504ce493338302d3d6eb56bb76409ea94f950e
Cesar Banchio <cesarbanchio@cesars-laptop.home>
2025-08-21 15:16:28 +0200

    changed production.rb again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
-  #config.assets.compile = false
-  #config.assets.digest = true
+  config.assets.compile = false
+  config.assets.digest = true
 
 
   # Full error reports are disabled.
